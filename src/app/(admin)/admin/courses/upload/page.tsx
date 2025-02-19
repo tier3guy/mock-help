57a5bc35c805e7a7486f719047fb718da4b160a4
harshagarwal7202 <harshagarwal7202@gmail.com>
2025-02-19 20:31:35 +0530

    Add Course Page

--- src/app/(admin)/admin/courses/upload/page.tsx
+++ src/app/(admin)/admin/courses/upload/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
-    return (
-        <div className="container">
-            <h1>Upload Page</h1>
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1>Upload Page</h1>
+    </div>
+  );
 }
